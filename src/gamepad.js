72132a6c5958f990fe1fafb32398c21d15e857b0
Alexey Pyasetskiy <apyasetskiy@cvent.com>
2020-09-21 07:50:35 +0300

    gamepad

---
+++ src/gamepad.js
@@ -0,0 +1,34 @@
+const HID = require('node-hid');
+
+const EventEmitter = require('events');
+
+const { GAMEPAD_VENDOR_ID, GAMEPAD_PRODUCT_ID } = require('./configs')
+
+const devices = HID.devices();
+
+const deviceInfo = devices.find( function(d) {
+    return d.vendorId===GAMEPAD_VENDOR_ID && d.productId===GAMEPAD_PRODUCT_ID;
+});
+
+const GamepadEmitter = new EventEmitter();
+
+if( deviceInfo ) {
+    var device = new HID.HID( deviceInfo.path );
+
+    device.on("data", function(bytes) {
+        const hex = bytes.toString("hex");
+        GamepadEmitter.emit('START_BACK', hex.slice(4,5));
+        GamepadEmitter.emit('DPAD', hex.slice(5,6));
+        GamepadEmitter.emit('BUTTON', hex.slice(6,8));
+        GamepadEmitter.emit('TRIGGER', bytes.readUIntBE(4, 1), bytes.readUIntBE(5, 1));
+        GamepadEmitter.emit('LEFT_STICK', bytes.readUIntBE(6, 2), bytes.readUIntBE(8, 2));
+        GamepadEmitter.emit('RIGHT_STICK', bytes.readUIntBE(10, 2), bytes.readUIntBE(12, 2));
+
+    });
+
+    device.on("error", function(err) {
+        console.log(err.toString());
+    });
+}
+
+module.exports.ControllerEvents = GamepadEmitter;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-const HID = require('node-hid');
-const colors = require('colors')
 
-const EventEmitter = require('events');
+const { ControllerEvents } = require('./gamepad');
 
-// const hexapod = require("./hexapod");
 const configs = require("./configs");
 const { solveInverseKinematics } = require('./hexapod/solvers/ik/hexapodSolver');
 const { getWalkSequence } = require("./hexapod/solvers/walkSequenceSolver");
 const { controllerCMD } = require('./hexapod/servo');
 
-const devices = HID.devices();
-
-const deviceInfo = devices.find( function(d) {
-    return d.vendorId===1133 && d.productId===49695;
-});
-
-const GamepadEmitter = new EventEmitter();
 
 const countSteps = sequence => sequence["leftMiddle"].alpha.length
 
@@ -28,27 +18,6 @@ const getPose = (sequences, i) => {
 }
 
 function main() {
-
-    if( deviceInfo ) {
-        var device = new HID.HID( deviceInfo.path );
-        
-        device.on("data", function(bytes) {
-            const hex = bytes.toString("hex");
-            console.debug(bytes);
-            GamepadEmitter.emit('START_BACK', hex.slice(4,5));
-            GamepadEmitter.emit('DPAD', hex.slice(5,6));
-            GamepadEmitter.emit('BUTTON', hex.slice(6,8));
-            GamepadEmitter.emit('TRIGGER', bytes.readUIntBE(4, 1), bytes.readUIntBE(5, 1));
-            GamepadEmitter.emit('LEFT_STICK', bytes.readUIntBE(6, 2), bytes.readUIntBE(8, 2));
-            GamepadEmitter.emit('RIGHT_STICK', bytes.readUIntBE(10, 2), bytes.readUIntBE(12, 2));
-
-        });
-        
-        device.on("error", function(err) {
-            console.log(err.toString());
-        });
-      
-      }
 }
 
 const reset = () => {
@@ -76,7 +45,7 @@ function move(isForward, inWalkMode) {
     console.log(step, cmd);
 }
 
-GamepadEmitter.on('START_BACK', (value) => {
+ControllerEvents.on('START_BACK', (value) => {
     switch(parseInt(value)) {
         case 1:
             // stadn up
@@ -98,20 +67,20 @@ GamepadEmitter.on('START_BACK', (value) => {
       }
 });
 
-GamepadEmitter.on('DPAD', (value) => {
+ControllerEvents.on('DPAD', (value) => {
     // console.log(`DPAD pressed ${value}`);
 });
 
-GamepadEmitter.on('BUTTON', (value) => {
+ControllerEvents.on('BUTTON', (value) => {
     // x y b a | X or logitech btn | top triggers LB and RB
     // console.log(`BUTTON pressed ${value}`);
 });
 
-GamepadEmitter.on('TRIGGER', (LTValue, RTValue) => {
+ControllerEvents.on('TRIGGER', (LTValue, RTValue) => {
     // console.log(`TRIGGER pressed ${LTValue}, ${RTValue}`);
 });
 
-GamepadEmitter.on('LEFT_STICK', (x_val, y_val) => {
+ControllerEvents.on('LEFT_STICK', (x_val, y_val) => {
     switch (true){
         case y_val < 10000:
             var moveId = setInterval(move, 1000, true, true);
@@ -127,7 +96,7 @@ GamepadEmitter.on('LEFT_STICK', (x_val, y_val) => {
     console.log(`LEFT_STICK pressed ${x_val}, ${y_val}`);
 });
 
-GamepadEmitter.on('RIGHT_STICK', (x_val, y_val) => {
+ControllerEvents.on('RIGHT_STICK', (x_val, y_val) => {
     console.log(`START_BACK pressed ${x_val}, ${y_val}`);
 });
 

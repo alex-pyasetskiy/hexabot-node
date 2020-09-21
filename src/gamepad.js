65642f698c378845f72a6ad76d3cfecc9a6900b7
Alexey Pyasetskiy <apyasetskiy@cvent.com>
2020-09-21 10:26:16 +0300

    dd

--- src/gamepad.js
+++ src/gamepad.js
@@ -4,9 +4,30 @@ usbDetect.startMonitoring();
 
 const { GAMEPAD_VENDOR_ID, GAMEPAD_PRODUCT_ID } = require('./configs')
 
+const GamepadEmitter = new EventEmitter();
 
 usbDetect.find(GAMEPAD_VENDOR_ID, GAMEPAD_PRODUCT_ID, function(err, devices) { 
     console.log(devices);
+    const deviceInfo = devices[0];
+
+    if( deviceInfo ) {
+        var device = new HID.HID( deviceInfo.vendorId, deviceInfo.productId );
+
+        device.on("data", function(bytes) {
+            const hex = bytes.toString("hex");
+            GamepadEmitter.emit('START_BACK', hex.slice(4,5));
+            GamepadEmitter.emit('DPAD', hex.slice(5,6));
+            GamepadEmitter.emit('BUTTON', hex.slice(6,8));
+            GamepadEmitter.emit('TRIGGER', bytes.readUIntBE(4, 1), bytes.readUIntBE(5, 1));
+            GamepadEmitter.emit('LEFT_STICK', bytes.readUIntBE(6, 2), bytes.readUIntBE(8, 2));
+            GamepadEmitter.emit('RIGHT_STICK', bytes.readUIntBE(10, 2), bytes.readUIntBE(12, 2));
+    
+        });
+    
+        device.on("error", function(err) {
+            console.log(err.toString());
+        });
+    }
 
 });
 // usbDetect.find(vid, function(err, devices) { console.log('find', devices, err); });
@@ -44,4 +65,4 @@ usbDetect.find(GAMEPAD_VENDOR_ID, GAMEPAD_PRODUCT_ID, function(err, devices) {
 //     });
 // }
 
-// module.exports.ControllerEvents = GamepadEmitter;
+module.exports.ControllerEvents = GamepadEmitter;
